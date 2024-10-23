dcbf0c07b316efeb9452a9e2ebda51c83329241d
utkarshshanu712 <shanuutkarsh712@gmail.com>
2024-10-23 06:45:04 +0000

    vc

---
+++ basics/datatype.js
@@ -0,0 +1,20 @@
+"use strict";
+let name="uk"
+//
+let age="17"
+//
+let loggedin = false
+//
+/*
+number => 2 to power 53
+big ant if number is very big
+sreing=>""or''
+boolean =>true/false
+number
+null=> standalone it's object
+symbol=> unique
+undifiend
+//object
+*/
+console.log(typeof "age")
+
---
+++ basics/vc.js
@@ -0,0 +1,17 @@
+
+const accountID = 123456
+//it is constant
+var passwaord = "123456"
+// var and let woark almost same but never use var because it is old
+let accountemail ="uk@gmail.com"
+accountcitycity ="gaya"
+//it is variable but it is not good practice
+let accountstate;
+//undefinded
+let temp="nulll"
+//standolone value (type of object)
+console.log(accountID,accountemail,passwaord);
+console.table([accountID,passwaord,accountemail,accountcitycity,accountstate,null])
+/*
+use console.table
+*/
