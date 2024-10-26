001c7c6ff67b8a98e5735ef6999c2a0e536be740
utkarshshanu712 <shanuutkarsh712@gmail.com>
2024-10-26 08:34:02 +0000

    conversion

---
+++ basics/conv.js
@@ -0,0 +1,22 @@
+let Name = "utkarsh";
+let valuInString = String(Name);
+console.log(typeof valuInString);
+console.log(valuInString);
+
+let score ="33"
+
+let valueInNumber =Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+let isLoggedIn="uk"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(typeof booleanIsLoggedIn,isLoggedIn)
+
+console.log(isLoggedIn);
+
+let somename = "36"
+
+let stringname = String(somename)
+console.log(stringname)
+console.log(typeof stringname);
