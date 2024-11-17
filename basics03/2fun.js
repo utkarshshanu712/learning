0c6a49247c4c9ad7d0dcff85fa4f1611adc75837
utkarshshanu712 <shanuutkarsh712@gmail.com>
2024-11-17 16:15:34 +0000

    scopes

---
+++ basics03/2fun.js
@@ -0,0 +1,27 @@
+function addtocart(val1,...num1){
+   return num1
+}
+console.log(addtocart(2000,3000,4000))
+ const user ={
+    username:"utkarsh",
+    price:1999,
+
+ }
+
+ function obj(anyobject){
+    console.log(`username is ${anyobject.username} and price is ${anyobject.price}`)
+
+ }
+ obj(user)
+ obj({
+    username:"sam",
+    price:"2rs"
+ })
+
+ const array1=[200,300,400,500]
+
+ function value(getarry){
+    return getarry[3]
+ }
+
+ console.log(value(array1))
---
+++ basics03/scopes.js
@@ -0,0 +1,27 @@
+let a= 300
+if(true){
+    let a=10
+    const b=20
+    console.log("ineer:", a)
+}
+console.log(a)
+
+function one(){
+    let useerkey =123456
+    function details(){
+        const website="google"
+        console.log(useerkey)
+        console.log(website)
+    }
+    
+    details()
+}
+one()
+
+if(true){
+    const username="uk"
+    if(true){
+        const website=" youtube"
+        console.log(username+website)
+    }
+}
